24b42f862b5855bb0c4b6bc554562dedc0f0808d
JAVA <yhk06262@naver.com>
2019-05-10 21:08:13 +0900

    Add ChatProgram_ver2

--- ChatProgram_ver2/ClientTestMain.java
+++ ChatProgram_ver2/ClientTestMain.java
@@ -3,5 +3,6 @@ package chapter17_Problem3_ver2;
 public class ClientTestMain {
 	public static void main(String[] args) {
 		new ClientView();
+		System.out.println("githup에 올리면 변경사항이 저장될까?? 궁금해서 바꾸는 코드");
 	}
 }
